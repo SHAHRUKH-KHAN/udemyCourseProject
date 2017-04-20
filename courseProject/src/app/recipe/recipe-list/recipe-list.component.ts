913c2280c8daeb6cc95823dfc13ffe094dce30f7
shahrukh khan <shahrukh.khan@accenture.com>
2017-04-20 16:06:11 +0530

    project skeleton

--- courseProject/src/app/recipe/recipe-list/recipe-list.component.ts
+++ courseProject/src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Simple Test', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/user-collections/my-colelction-image/2015/12/recipe-image-legacy-id--1274488_8.jpg?itok=vS12pL7b'),
+    new Recipe('Test Recipe', 'Second one', 'http://images.indianexpress.com/2014/07/masala-baby-potatoes.jpg?w=820?w=608')
+  ];
 
   constructor() { }
 
---
+++ courseProject/src/app/shared/ingredients.model.ts
@@ -0,0 +1,13 @@
+export class Ingredients {
+    /*public name: string;
+    public amount: number;
+
+    constructor(name:string, amount: number) {
+        this.name = name;
+        this.amount = amount;    
+    }*/
+
+    /*There is short-cut for above implementation*/
+
+    constructor(public name:string, public amount: number) {}
+}
